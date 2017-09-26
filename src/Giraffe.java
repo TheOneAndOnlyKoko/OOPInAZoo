2e38539ea0f2b50401af832934fbb540edc0893e
TheOneAndOnlyKoko <kostyanatsoulis@students.berkeley.net>
2017-09-26 10:32:35 -0700

    Bee Giraffe Unicorn initialize

--- src/Giraffe.java
+++ src/Giraffe.java
@@ -2,5 +2,24 @@
  * Created by h205p2 on 9/26/17.
  */
 public class Giraffe {
+    String name;
+    String favoriteFood;
 
+    public Giraffe(String name, String favoriteFood) {
+        // put your constructor content here
+        super(name, favoriteFood);
+    }
+
+    public void sleep() {
+        // complete your sleep function here, noting the change from global to instance variables
+        System.out.println(name + " sleeps for 8 hours");
+    }
+
+    public void eat(String food) {
+        // complete your eat function here!
+        if (food == favoriteFood) {
+            System.out.println(name + " eats " + food);
+            System.out.println("YUM!!! " + name + " wants more " + food);
+        }
+    }
 }
--- src/Unicorn.java
+++ src/Unicorn.java
@@ -1,7 +1,19 @@
 /**
  * Created by h205p2 on 9/26/17.
  */
-public class Unicorn {
+// Implement the Unicorn class here as a subclass of Animal
+// Hint: Implement the initializer method and override the sleep method
+public class Unicorn extends Animal {
+    String name;
+    String favoriteFood;
+    public Unicorn(String name, String favoriteFood) {
+        // don't forget to correct the call to the superclass initializer!
+        super(name, favoriteFood);
+    }
 
-
+    public void sleep() {
+        // your overridden sleep code...
+        System.out.println(name + " sleeps on a cloud.");
+    }
 }
+
